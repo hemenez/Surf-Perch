2f37085e5d05edd5efec04bcc7a7b185e4feb5be
Yunju <yunju@Yunjus-MacBook-Air.local>
2018-05-18 13:56:30 -0700

    Send search keyword

--- template/script.js
+++ template/script.js
@@ -1,8 +1,10 @@
 let submit = document.getElementById("submit");
+let search = document.getElementById("search");
 submit.addEventListener("click", function(event) {
+  console.log(search.value);
   event.preventDefault();
 });
 
 $('#twitter-connect').click(function() {
-  do something...
+  console.log('')
 });
