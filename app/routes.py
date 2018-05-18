e2adf72fd67d2a26a863945349ee4e084ba5febf
romalms10 <romalms10@gmail.com>
2018-05-18 22:08:40 +0000

    Create API endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
 from app import app
+from flask import jsonify, request
+import json
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+@app.route('/save_keyword', strict_slashes=False, methods=['GET', 'POST'])
+def keyword():
+    """
+    Save the keyword to a file in a POST request
+    Serve the keyword to a GET request
+    """
+    if request.method == 'POST':
+        post_dict = request.get_json()
+        with open('file.json', 'w') as f:
+            json.dump(post_dict, f)
+
+    if request.method == 'GET':
+        with open('file.json') as f:
+            data = json.load(f)
+        return jsonify(data)
