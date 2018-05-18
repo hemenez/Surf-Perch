1f557c35a1795759a780e48fd11ac9338c0c44c1
Yunju <yunju@Yunjus-MacBook-Air.local>
2018-05-18 12:39:56 -0700

    Add twitter button and search page

---
+++ template/script.js
@@ -0,0 +1,8 @@
+let submit = document.getElementById("submit");
+submit.addEventListener("click", function(event) {
+  event.preventDefault();
+});
+
+$('#twitter-connect').click(function() {
+  do something...
+});
